a4a3ba7c7a77d06c6f7c04ed289294dd391132de
Manimegalai <manimegalai30102@gmail.com>
2023-08-08 22:25:09 +0530

    final code

--- app.js
+++ app.js
@@ -1,31 +1,53 @@
-function fetchAbilityName() {
-    const url='https://pokeapi.co/api/v2/ability/';
+async function fetchData(url){
+  try{
+    let response= await fetch(url); 
+    if(!response.ok){
+      console.log(response);
+    }
+    return response.json();
+  }
+  catch(error){
+    console.log(error.message);
+}  
+} 
 
-    return fetch(url)
-    .then(res => {
-       return res.json();
-    })
+async function fetchabilityName(){
+  const url='https://pokeapi.co/api/v2/ability';
+  try{
+    const data= await fetchData(url);  
+  
+    let selectedItem=document.getElementById("selected");
+    let paraEle=document.getElementById("para");
+    const ability=data.results;
+    ability.forEach((item)=>{
+         let option=document.createElement("option");
+         option.innerText = item.name;
+         selectedItem.appendChild(option)
+         });
 
-    .then(msg => {
+         selectedItem.addEventListener('change',async()=>{
+           paraEle.innerText=" ";
+           let selectedName=selectedItem.value;
+           const selectedAbility=ability.find(item1 => item1.name === selectedName)
 
-     const ability=msg.results;
-     let selectedItem=document.getElementById("selected");
+           if(selectedAbility){
+            const urlData= await fetchData(selectedAbility.url)
+            console.log(urlData)
+            let para1=document.createElement("li");
+       
+            para1.innerHTML=urlData.flavor_text_entries[0].flavor_text;
+            paraEle.appendChild(para1);
+           }
+          })
+  }
+  catch(error){
+    let err=document.getElementById("errormsg")
+    err.style.display="block"
+};
+}
+fetchabilityName();
 
-     ability.forEach((item)=>{
-     let option=document.createElement("option");
-     option.innerText = item.name;
-     selectedItem.appendChild(option)
-     });
-
-    })
-    .catch( error => { 
-     console.log('Error', error);
-   let err=document.getElementById("errormsg")
-   err.style.display="block"
 
 
-   return  null;
-    });
-   
- }
- fetchAbilityName()
+
+ 
